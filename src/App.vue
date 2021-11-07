a928c69c1f18efe78d0284ed8d196fb43b0ea381
cybergaulker <cybergaukler@gmail.com>
2021-11-07 09:21:07 +0100

    base building

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,69 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import Scene from './components/Scene.vue'
+  import Configuration from './components/Configuration.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="navigation">
+    <div class="logo"><span>VRKSpace&nbsp;</span><sup>[vɛʁkspeɪs]</sup></div>
+    <div class="links">
+        <a href="https://github.com/cybergaukler/vrkspace/wiki" target="_blank"><i class="fab fa-github-square"></i>&nbsp;Philosophy&More</a>
+    </div>
+  </div>
+  <div class="scene">
+    <Scene />
+  </div>
+  <Configuration class="configuration"/>
 </template>
 
 <style>
+  body, html {
+    position: relative;    
+    height: 100%;
+    margin: 0 auto;
+  }
+  .navigation {
+    position: absolute;
+    z-index:2;
+    width:100%;
+    height: 2em;
+    font-size: 1em;
+    left:0;
+    padding: 0.2em;
+    background-color: #555555;
+    color: #a0a0a0;
+  }
+  .navigation .logo {
+    float: left;
+    margin-left:0.2em;
+  }
+  .navigation .logo span{
+      font-weight: bold;
+      color: #F8F8F8;
+  }
+  .navigation .links {
+      float: right;
+      margin-right: 8em;
+  }
+  .navigation .links a {
+      display: block;
+      float: left;
+      margin-left: 2em;
+      color: orange;
+  }
+  .navigation .links a:hover {
+      color: #F8F8F8;
+  }
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    position:relative;
+    height:100%;
+  }
+  .scene {
+    height: 66%
+    }
 </style>
---
+++ src/components/Configuration.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
+
+<template>
+         <div class="content">
+            <div class="configHead">
+                <span>Before you take a seat...</span>
+                <div class="selectConfig">
+                    <select id="selectedConfig" title="select your configuration">
+                        <option value="Default" selected="selected">Please configure</option>
+                        <option value="HTCViveBasic">Vive Basic</option>
+                    </select>
+                </div>
+            </div>
+            <div class="configBody">
+
+                <div class="configSteps None">
+                    Hello VRKSPace
+                </div>
+
+                <div class="configSteps HTCViveBasic">
+                    Please place the VIVE Controllers on either side of your physical desk, close to the front edge as shown above.<br />
+                    <ul>
+                        <li>The controllers must be active.</li>
+                        <li>Left Controller should be on the left side of the desk, right controller on the right side.</li>
+                    </ul>
+                    Then click enterVR with your mouse and put on the headset.<br />
+                    In VR, to align the desk to your physical desk, push both GRIP and MENU on both controllers simultaneously.
+                </div>
+
+            </div>
+
+        </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
